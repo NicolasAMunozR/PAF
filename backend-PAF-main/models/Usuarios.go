ea4bef0fd5f98d0381c989a5c92d7c2c5cd3f9a9
haruhisomu <nicolas.salinas.r@usach.cl>
2024-12-06 22:02:09 -0300

    tuki

--- backend-PAF-main/models/Usuarios.go
+++ backend-PAF-main/models/Usuarios.go
@@ -4,4 +4,6 @@ type Usuarios struct {
 	Run    string `gorm:"type:text"`
 	Nombre string `gorm:"type:text"`
 	Rol    string `gorm:"type:text"`
+	//se añadio este parametro
+	UnidadMayor string `gorm:"type:text"`
 }
