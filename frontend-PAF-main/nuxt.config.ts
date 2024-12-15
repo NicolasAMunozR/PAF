d53cc31721ed0902939f36e15e038ed2b5fcf02c
Nicolás Muñoz <nicolas.munoz.r@usach.cl>
2024-12-15 15:19:58 -0300

    A

--- frontend-PAF-main/nuxt.config.ts
+++ frontend-PAF-main/nuxt.config.ts
@@ -1,14 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-    devtools: { enabled: true },
-    modules: ['@nuxtjs/tailwindcss'],
-    runtimeConfig: {
-      public: {
-        apiBase: 'http://localhost:3000' // Cambia esta URL según tu entorno
-      }
-    },
-    devServer: {
-      port: 3001
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:3000' // Cambia esta URL según tu entorno
     }
-  })
+  },
+  devServer: {
+    port: 3001
+  },
+  plugins: [
+    '~/plugins/extend-routes.ts', // Asegúrate de que el archivo de plugin se registre aquí
+  ]
+})
