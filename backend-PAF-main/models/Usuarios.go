9179721fc21bf31e7a5d63bd1c418cb59ee3e144
haruhisomu <nicolas.salinas.r@usach.cl>
2024-12-23 17:25:08 -0300

    cambio requerido

--- backend-PAF-main/models/Usuarios.go
+++ backend-PAF-main/models/Usuarios.go
@@ -8,4 +8,10 @@ type Usuarios struct {
 	UnidadMayor string `gorm:"type:text"`
 	//se añadio este parametro
 	UnidadMenor string `gorm:"type:text"`
+	  // Se agregan los siguientes campos
+    Cod_umayor int gorm:"type:integer"
+    Cod_umenor int gorm:"type:integer"
+    acceso int gorm:"type:integer"
+    vista_universidad int gorm:"type:integer"
+    vista_facultad int gorm:"type:integer"
 }
