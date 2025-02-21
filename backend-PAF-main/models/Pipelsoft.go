f75f527609faf19c8a3aadb21e43921dc6eaa63a
haruhisomu <nicolas.salinas.r@usach.cl>
2025-02-21 15:13:45 -0300

    actualizacion modelo pipelsoft

--- backend-PAF-main/models/Pipelsoft.go
+++ backend-PAF-main/models/Pipelsoft.go
@@ -34,11 +34,40 @@ type Pipelsoft struct {
 	Categoria string `gorm:"type:text"`
 
 	// Campos de Proceso
-	CodEstado string `gorm:"type:text"`
-	DesEstado string `gorm:"type:text"`
-	Llave     string `gorm:"type:text"`
-	Veces     string `gorm:"type:text"`
-	Planta    string `gorm:"type:text"`
+	CodEstado                    string `gorm:"type:text"`
+	DesEstado                    string `gorm:"type:text"`
+	Llave                        string `gorm:"type:text"`
+	Veces                        string `gorm:"type:text"`
+	Planta                       string `gorm:"type:text"`
+	NumeroCentroDeCostos         string `gorm:"type:text"`
+	LugarNacimiento              string `gorm:"type:text"`
+	Nacionalidad                 string `gorm:"type:text"`
+	Domicilio                    string `gorm:"type:text"`
+	Correo                       string `gorm:"type:text"`
+	Titulo                       string `gorm:"type:text"`
+	Institucion                  string `gorm:"type:text"`
+	FechaObtencion               string `gorm:"type:text"`
+	NumeroSemestre               string `gorm:"type:text"`
+	GradoAcademico               string `gorm:"type:text"`
+	InstitucionGradoAcademico    string `gorm:"type:text"`
+	FechaObtencionGradoAcademico string `gorm:"type:text"`
+	TipoIngreso                  string `gorm:"type:text"`
+	Cargo                        string `gorm:"type:text"`
+	Nivel                        string `gorm:"type:text"`
+	Grado                        string `gorm:"type:text"`
+	Rango                        string `gorm:"type:text"`
+	Funcion                      string `gorm:"type:text"`
+	Asignatura                   string `gorm:"type:text"`
+	NumeroHoras                  string `gorm:"type:text"`
+	Calidad                      string `gorm:"type:text"`
+	LugarDesempeño               string `gorm:"type:text"`
+	CargoOtroPublico             string `gorm:"type:text"`
+	GradoOtroPublico             string `gorm:"type:text"`
+	NivelOtroPublico             string `gorm:"type:text"`
+	RangoOtroPublico             string `gorm:"type:text"`
+	NumeroHorasOtroPublico       string `gorm:"type:text"`
+	CalidadOtroPublico           string `gorm:"type:text"`
+
 	//añadir dos campos solicitados
 	// y posiblemente añadir mas logica de negocio
 
